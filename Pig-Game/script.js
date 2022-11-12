f71dc5531e8eec1f468e5c86a3a7673b549a7fa1
Alexander Iordanou <iordanoualexander@gmail.com>
2022-11-12 20:44:30 +0200

    Fixes bug with new game button. Severely refactors code

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -3,10 +3,10 @@
 /*
 Variable declaration
 */
-let activePlayer = document.querySelector('.player--active'); // The player whose turn it is to play. Always start with player 1
-const playerTurn = document.querySelectorAll('.player'); //array of the two players player1 = 0, player2 = 1
-const currentScore = document.querySelectorAll('.current-score'); //same as above
-const overallScore = document.querySelectorAll('.score'); //same as above
+const playerTurn = document.querySelectorAll('.player'); //Nodelist of the two players player1 = 0, player2 = 1
+const currentScore = document.querySelectorAll('.current-score'); //same as above fr current score
+const overallScore = document.querySelectorAll('.score'); //same as above for overall score
+let activePlayerIndex = 0; // The player whose turn it is to play. Always start with player 1 = 0
 const GAMEOVERSCORE = 50;
 //-----------------------------------------------------------
 const rollDiceBtn = document.querySelector('.btn--roll'); //roll dice button
@@ -14,120 +14,110 @@ const holdBtn = document.querySelector('.btn--hold'); // Hold button. Saves Poin
 const newGameBtn = document.querySelector('.btn--new'); // Restart the game button
 //-----------------------------------------------------------
 const diceImg = document.querySelector('.dice'); //Image of the dice displayed
+let isPlaying = true; //the state of the game. Ongoing or over.
 //-----------------------------------------------------------
+
+const getActivePlayerIndex = function () {
+  //Helper function to retrieve the index of the active player.
+  if (playerTurn[0].classList.contains('player--active')) {
+    activePlayerIndex = 0;
+  } else {
+    activePlayerIndex = 1;
+  }
+  return activePlayerIndex;
+};
 
 const changeActivePlayer = function () {
   /*
   Function that changes the active player from player 1, to player 2
   and reverse.
   */
-  if (activePlayer.classList.contains('player--0')) {
-    activePlayer.classList.remove('player--active');
-    const newActivePlayer = playerTurn[1];
-    newActivePlayer.classList.add('player--active');
-    activePlayer = newActivePlayer;
-  } else {
-    activePlayer.classList.remove('player--active');
-    const newActivePlayer = playerTurn[0];
-    newActivePlayer.classList.add('player--active');
-    activePlayer = newActivePlayer;
-  }
-  return activePlayer;
+  playerTurn.forEach(element => {
+    element.classList.toggle('player--active');
+  });
 };
+
 //-----------------------------------------------------------
 
-const calcCurrentScore = function (playerIndex, roll) {
+const calcCurrentScore = function (roll) {
   /*
- Helper Function that takes the player's index (0 or 1) and the roll.
- Calculates the current score for that player.
+ Helper Function that takes the roll and
+ calculates the current score for the active player.
   */
-  currentScore[playerIndex].textContent = `${
-    Number(currentScore[playerIndex].textContent) + roll
+  currentScore[activePlayerIndex].textContent = `${
+    Number(currentScore[activePlayerIndex].textContent) + roll
   }`;
 };
 //-----------------------------------------------------------
 
-const rollDice = function (player) {
+const rollDice = function () {
   /*helper function. roll a dice for the active player and get the appropriate png, and the actual diceroll.
-    if the roll is equal to 1, the current score and the overall score become 0.
+    if the roll is equal to 1, the current score becomes 0.
     Additionally, the active player loses their turn and the other player becomes active
   */
   const num = Math.round(Math.random() * 5 + 1);
+  getActivePlayerIndex();
   diceImg.src = `dice-${num}.png`;
-  if (player.classList.contains('player--0')) {
-    if (num === 1) {
-      currentScore[0].textContent = '0';
-      overallScore[0].textContent = '0';
-      changeActivePlayer();
-    } else {
-      calcCurrentScore(0, num);
-    }
+
+  if (num === 1) {
+    currentScore[activePlayerIndex].textContent = '0';
+    changeActivePlayer();
   } else {
-    if (num === 1) {
-      currentScore[1].textContent = '0';
-      overallScore[1].textContent = '0';
-      changeActivePlayer();
-    } else {
-      calcCurrentScore(1, num);
-    }
-    return diceImg;
+    calcCurrentScore(num);
   }
+  return diceImg;
 };
 //-----------------------------------------------------------
 
 function toggleButtons() {
-  rollDiceBtn.disabled = rollDiceBtn.disabled ? false : true;
-  holdBtn.disabled = holdBtn.disabled ? false : true;
+  //helper function. Depending on the state of the game "isPlaying" ,
+  //the roll dice and hold buttons get enabled or disabled.
+
+  rollDiceBtn.disabled = isPlaying ? false : true;
+  holdBtn.disabled = isPlaying ? false : true;
 }
 //-----------------------------------------------------------
 
 function checkWinner() {
   /*
     helper function. Checks if there is a winner and displays a message.
-    Additionally, disables all buttons except new game.
-    Game ends at 50 points currently.
+    Additionally,game is over and disables all buttons except new game.
   */
-  if (Number(overallScore[0].textContent) >= GAMEOVERSCORE) {
+
+  if (Number(overallScore[activePlayerIndex].textContent) >= GAMEOVERSCORE) {
+    isPlaying = false;
     toggleButtons();
-    alert(`Player 1 Wins!!!`);
-  } else if (Number(overallScore[1].textContent) >= GAMEOVERSCORE) {
-    toggleButtons();
-    alert(`Player 2 Wins!!!`);
+    alert(`Player ${activePlayerIndex + 1} Wins!!!`);
   }
 }
 //-----------------------------------------------------------
 
 //add event on click of "Roll Dice" Button.
-rollDiceBtn.addEventListener('click', function () {
-  rollDice(activePlayer);
-});
+rollDiceBtn.addEventListener('click', rollDice);
 
 /*add event on click to Hold button. We sum the overall
-  score with the current score, check for a winner and change
+  score with the current score, check for a winner, and change
   active player.
 */
 holdBtn.addEventListener('click', function () {
-  if (activePlayer.classList.contains('player--0')) {
-    overallScore[0].textContent = `${
-      Number(overallScore[0].textContent) + Number(currentScore[0].textContent)
-    }`;
-    currentScore[0].textContent = '0';
-  } else {
-    overallScore[1].textContent = `${
-      Number(overallScore[1].textContent) + Number(currentScore[1].textContent)
-    }`;
-    currentScore[1].textContent = '0';
-  }
+  getActivePlayerIndex();
+
+  overallScore[activePlayerIndex].textContent = `${
+    Number(overallScore[activePlayerIndex].textContent) +
+    Number(currentScore[activePlayerIndex].textContent)
+  }`;
+  currentScore[activePlayerIndex].textContent = '0';
+
   checkWinner();
   changeActivePlayer();
 });
 
 /*
   Add event to new game button on click.
-  Initialize all values and enable all buttons.
+  Initialize game and enable all buttons.
 */
+
 newGameBtn.addEventListener('click', function () {
-  toggleButtons();
   currentScore.forEach(element => {
     element.textContent = '0';
   });
@@ -135,9 +125,12 @@ newGameBtn.addEventListener('click', function () {
   overallScore.forEach(element => {
     element.textContent = '0';
   });
-  activePlayer.classList.remove('player--active');
-  const startPlayer = document.querySelector('.player--0');
-  startPlayer.classList.add('player--active');
-  activePlayer = startPlayer;
+
+  playerTurn[activePlayerIndex].classList.remove('player--active');
+  playerTurn[0].classList.add('player--active');
+  activePlayerIndex = 0;
+
   diceImg.src = 'dice-6.png';
+  isPlaying = true;
+  toggleButtons();
 });
